593b2a7547c4c1a8c035656c6ae2d1efc915b2dd
zzeordyga <47037524+zzeordyga@users.noreply.github.com>
2021-03-30 00:57:03 +0700

    #development creating basic player movement

---
+++ Assets/Self Made Assets/Code/MouseLook.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Locks the Cursor in the middle
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Gets the mouse input based on the sensitivity and adjusted with the frame rate
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Rotation on X axis (increasing will result in flipping the rotation)
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Self Made Assets/Code/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    #region Movement Variables
+    public float speed = 12f;
+    public bool isRunning = false;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    #endregion
+
+    #region Ground Checking Utils
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    #endregion
+
+    #region Everchanging Variables
+    Vector3 velocity;
+    bool isGrounded;
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Checks using a made sphere with a radius if a certain layer is hit (in this case, Ground Layer)
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Resets the speed of our fall
+        if(isGrounded && velocity.y < 0)
+        {
+            // Not zero so that we don't immediately reset speed upon colliding with ground
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetKey(KeyCode.LeftShift)) isRunning = true;
+        else isRunning = false;
+
+        if (isRunning) speed = 18f;
+        else speed = 12f;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Formula for jumping
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y = gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+}
