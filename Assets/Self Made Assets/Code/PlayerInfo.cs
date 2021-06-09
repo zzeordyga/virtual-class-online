aa0fbe1ba366a5e83a7fe3ffaf8d14ca0c12e46c
xtream-00 <joseearmandoo26@gmail.com>
2021-06-09 20:20:50 +0700

    added login system & classroom scene

---
+++ Assets/Self Made Assets/Code/PlayerInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo
+{
+    public string BinusianId;
+    public string Major;
+    public string Name;
+    public string Role;
+    public string UserId;
+    public string UserName;
+}
